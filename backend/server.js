7ef5e45ec3bb8df016f70d7c575fe05d4900fb0d
Vo Nhat Nam <nhatnam23012004@gmail.com>
2026-04-02 14:36:05 +0700

    Final Commit - DevOps Project VoNhatNam

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors()); // Cho phép Frontend gọi API
+
+// Endpoint: /health
+app.get('/health', (req, res) => {
+    res.json({ status: "ok", app_name: process.env.APP_NAME || "DevOps-App" });
+});
+
+// Endpoint: /about (Thay thông tin của Nam vào đây)
+app.get('/about', (req, res) => {
+    res.json({
+        name: "Võ Nhật Nam",
+        id: "2251220036", 
+        class: "Lớp 22CT1"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Backend is running on port ${port}`);
+});
